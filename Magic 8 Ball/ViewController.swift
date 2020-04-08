5040751cf5399f6229e9b7b24a127006ff3f67b1
Colby Allen <colbyallen012@gmail.com>
2020-04-08 10:25:14 -0500

    add functionality to ask button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = #imageLiteral(resourceName: "ball4")
     }
     
     
